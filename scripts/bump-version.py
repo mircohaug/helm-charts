d7d83e9cecd8bebf41fb4c78d162d8273df3767d
Mirco Hacker <mirco.hacker@codecentric.de>
2021-12-23 12:03:52 +0100

    test on github
    Signed-off-by: Mirco Hacker <mirco.hacker@codecentric.de>

--- scripts/bump-version.py
+++ scripts/bump-version.py
@@ -27,14 +27,12 @@ def handle_subprocess_error(subprocess_result, error_message):
         print(str(subprocess_result.stdout, "UTF-8"))
         exit(1)
 
-
 if __name__ == "__main__":
     print(f"Arguments count: {len(sys.argv)}")
     for i, arg in enumerate(sys.argv):
         print(f"Argument {i:>6}: {arg}")
     chart_name = sys.argv[1]
     print(f"Bumping version for: {chart_name}")
-    # print(subprocess.run(["pwd"], capture_output=True))
     get_commits_process = subprocess.run(["/bin/bash", "-c",
                                           f"git log --pretty=format:%s $(git tag --list --sort=-version:refname '{chart_name}-*' | head -n 1)..HEAD ."],
                                          cwd=f"../charts/{chart_name}", capture_output=True)
@@ -52,7 +50,7 @@ if __name__ == "__main__":
             upgrade_type = UpgradeType.PATCH
 
     if upgrade_type == UpgradeType.NONE:
-        # TODO signal no release to workflow
+        print("::set-output name=publish::false")
         exit(0)
 
     print(upgrade_type)
@@ -61,4 +59,4 @@ if __name__ == "__main__":
                                     capture_output=True)
     handle_subprocess_error(subprocess_run, "Could not execute version bump")
 
-    print(str(subprocess_run.stdout, "UTF-8"))
+    print("::set-output name=publish::false")
